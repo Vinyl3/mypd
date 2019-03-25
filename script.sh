cf662443f629b924a9089fa21c68c5312c13a82a
Pratyush Kumar <pratyush.pk3@gmail.com>
2019-03-25 18:24:25 +0530

    change

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 # code to deploy angular app
 npm install
 cp -a /home/pratyush/azagent/_work/r2/a/Vinyl3.mypd/drop/. /home/pratyush/
-#nohup ng serve --host 0.0.0.0 &
+cd /home/pratyush
+nohup ng serve --host 0.0.0.0 &
