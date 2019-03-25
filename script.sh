14b57d5ca4776ed9a652e80e14d875524471e0fa
Pratyush Kumar <pratyush.pk3@gmail.com>
2019-03-25 20:04:03 +0530

    change

--- script.sh
+++ script.sh
@@ -2,5 +2,4 @@
 cp -a /home/pratyush/azagent/_work/r2/a/Vinyl3.mypd/drop/. /home/pratyush/
 cd /home/pratyush
 npm install
-npm start
-#nohup ng serve --host 0.0.0.0 &
+nohup ng serve --host 0.0.0.0 &
